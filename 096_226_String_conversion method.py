2ea57380433f4eb41715f458b0fa2676044fc24b
dnpy001 <dnpy001@gmail.com>
2020-10-08 14:55:05 +0530

    string-upper,lower,title

---
+++ 096_226_String_conversion method.py
@@ -0,0 +1,7 @@
+# 096_226_String_conversion method.py
+name = "hello"
+print("upper=", name.upper())
+print("title=", name.title())
+print("lower=", name.lower())
+print("swap=", name.swapcase())
+print("Happy Coding")
